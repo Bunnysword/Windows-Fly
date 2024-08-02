9a3dbb49a248a993e21c2144b5b98a38a51339de
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-08-02 19:01:09 +0300

    Update windows_fly.ps1

--- windows_fly.ps1
+++ windows_fly.ps1
@@ -1,7 +1,7 @@
 #
 winget install Transmission.Transmission Valve.Steam Discord.Discord Logitech.GHUB Yandex.Browser Nvidia.GeForceExperience Microsoft.VisualStudioCode SomePythonThings.WingetUIStore fastfetch
 #
-winget remove Microsoft.OneDrive MSIX\Microsoft.WindowsNotepad_11.2112.32.0_x64__8wekyb3d8bbwe MSIX\Microsoft.WindowsCalculator_10.2103.8.0_x64__8wekyb3d8bbwe MSIX\Microsoft.WindowsSoundRecorder_1.0.42.0_x64__8wekyb3d8bbwe MSIX\Microsoft.WindowsMaps_1.0.39.0_x64__8wekyb3d8bbwe MSIX\A025C540.Yandex.Music_4.54.8517.0_x64__vfvw9svesycw6 MSIX\Microsoft.Todos_0.54.42772.0_x64__8wekyb3d8bbwe MSIX\Microsoft.OneDriveSync_24141.714.3.0_neutral__8wekyb3d8bbwe MSIX\Microsoft.Paint_11.2201.22.0_x64__8wekyb3d8bbwe MSIX\Microsoft.PowerAutomateDesktop_1.0.219.0_x64__8wekyb3d8bbwe 
+winget remove MSIX\Microsoft.WindowsNotepad_11.2112.32.0_x64__8wekyb3d8bbwe MSIX\Microsoft.WindowsCalculator_10.2103.8.0_x64__8wekyb3d8bbwe MSIX\Microsoft.WindowsSoundRecorder_1.0.42.0_x64__8wekyb3d8bbwe MSIX\Microsoft.WindowsMaps_1.0.39.0_x64__8wekyb3d8bbwe MSIX\A025C540.Yandex.Music_4.54.8517.0_x64__vfvw9svesycw6 MSIX\Microsoft.Todos_0.54.42772.0_x64__8wekyb3d8bbwe MSIX\Microsoft.Paint_11.2201.22.0_x64__8wekyb3d8bbwe MSIX\Microsoft.PowerAutomateDesktop_1.0.219.0_x64__8wekyb3d8bbwe 
 #fastfetch
 New-Item -Path "c:\ProgramData\" -Name "fastfetch" -ItemType "directory"
 Move-Item -Path config.jsonc -Destination C:\ProgramData\fastfetch\config.jsonc
